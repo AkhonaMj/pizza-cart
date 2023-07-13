ab2315ba5f7ce31556c8757fd51a1de4c95a779f
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-07-13 09:30:15 +0200

    -

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-function PizzaCart(){
-    return {
-        smallCost : 0.00,
-        mediumCost : 0.00,
-        largeCost : 0.00,
-        totalCost : 0.00,
-
-
-        addSmallPizza(){
-            this.smallCost += 29.99
-            this.totalCost += 29.99
-        },
-        addMediumPizza(){
-            this.mediumCost+= 69.99
-            this.totalCost += 69.99
-        },
-        addLargePizza(){
-            this.largeCost += 99.99
-            this.totalCost += 99.99
-
-        },
-
-        checkout(){
-            
-        }
-
-
-    }
-}
+document.addEventListener("alpine:init", ()=>{
+    Alpine.data("Pizza", PizzaCart)
+});
