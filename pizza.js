a68e001fb691a736449b2894faa4d92550479959
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-07-14 09:37:03 +0200

    -

--- pizza.js
+++ pizza.js
@@ -1,37 +1,79 @@
-function PizzaCart(){
+function PizzaCart() {
     return {
-        smallCost : 0.00,
-        mediumCost : 0.00,
-        largeCost : 0.00,
-        totalCost : 0.00,
-        payCost : 0,
-        message : "",
+        smallCost: 0.00,
+        mediumCost: 0.00,
+        largeCost: 0.00,
+        totalCost: 0.00,
+        payCost: 0,
+        message: "",
+        change: 0,
 
 
-        addSmallPizza(){
+        addSmallPizza() {
             this.smallCost += 29.99
             this.totalCost += 29.99
         },
-        addMediumPizza(){
-            this.mediumCost+= 69.99
+
+        removeSmallPizza() {
+            if (this.smallCost >= 29.99) {
+                this.smallCost -= 29.99
+                this.totalCost -= 29.99
+            }
+
+        },
+
+        addMediumPizza() {
+            this.mediumCost += 69.99
             this.totalCost += 69.99
         },
-        addLargePizza(){
+
+        removeMediumPizza() {
+            if (this.mediumCost >= 69.99) {
+                this.mediumCost -= 69.99
+                this.totalCost -= 69.99
+
+            }
+        },
+        addLargePizza() {
             this.largeCost += 99.99
             this.totalCost += 99.99
 
         },
+        removeLargePizza() {
+            if (this.largeCost >= 99.99) {
+                this.largeCost -= 99.99
+                this.totalCost -= 99.99
 
-        checkout(){
-            if(this.payCost < this.totalCost){
+            }
+        },
+
+        checkout() {
+            if (this.payCost < this.totalCost) {
                 this.message = "Not enough money"
+
             }
 
-            else if(this.payCost >= this.totalCost){
-               this.message = "Enjoy your pizza!"
+            else if (this.payCost >= this.totalCost) {
+                this.message = "Enjoy your pizza!"
+                this.clearMessage()
+                this.smallCost = 0
+                this.mediumCost = 0
+                this.largeCost = 0
+                this.change = this.payCost - this.totalCost
+                this.totalCost = 0
+
+
             }
-        }
+        },
+        clearMessage() {
+            
+            setTimeout(()  => {
+                this.message = "";
+                console.log("hello");
+            }, 3000)
+        },
 
 
     }
+
 }
